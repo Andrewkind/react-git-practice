eef5aaff43ea4dfe36633bd6c9441f37def4dfde
Andrew Kind <andrew.w.kind@gmail.com>
2020-07-30 11:31:28 -0600

    Add Contact component and content.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './App.css';
 import About from './components/About';
 import Services from './components/Services';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
       <h1>Git Branching / Merge Practice</h1>
-      <p>Let's practice Git Branching and Merging!</p>
+      <p>Let's practice fantastic Git Branching and Merging!</p>
       <About />
       <Services />
+      <Contact />
+
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+    render() {
+        return (
+            <>
+                <h2>Contact</h2>
+                <p>Please reach out to work with us or if you have any questions. You can reach us through...</p>
+
+
+                <dl>
+                    <dt>Phone</dt>
+                    <dd>+1 (780) 123-4567</dd>
+                    <dt>E-Mail</dt>
+                    <dd>hello@example.com</dd>
+                </dl>
+            </>
+        )
+    }
+}
+
+export default Contact
