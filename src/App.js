610d79204818c455976e78e960365e6d5007acfd
Andrew Kind <andrew.w.kind@gmail.com>
2020-07-30 10:17:32 -0600

    Add content to App component.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Git Branching / Merge Practice</h1>
+      <p>Let's practice Git Branching and Merging!</p>
+    </div>
+  );
+}
+
+export default App;
